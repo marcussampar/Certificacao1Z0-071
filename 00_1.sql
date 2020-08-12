62321fc54948aa1e9ccde40a0b3b668d54ce5d31
Marcus Sampar <mvsampar@gmail.com>
2020-08-11 21:43:06 -0300

    Insert/Creates

--- 00_1.sql
+++ 00_1.sql
@@ -11,5 +11,10 @@ insert into funcionario values (4,'Fabio',27);
 insert into funcionario values (5,'Antonio',50);
 insert into funcionario values (6,'Marcus',29);
 insert into funcionario values (7,'Marcus',30);
-
+insert into funcionario values (9,'',32);
+insert into funcionario values (10,' ',32);
+insert into funcionario values (12,'MA',40);
+insert into funcionario values (13,'M',41);
+insert into funcionario values (14,'MAC',42);
+insert into funcionario values (15,'M_ARCUS',32);
 
