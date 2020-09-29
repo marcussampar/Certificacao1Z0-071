2694a538408e21b72dd8e32c8c998d40d110d799
Marcus Sampar <mvsampar@gmail.com>
2020-09-29 12:54:54 -0300

    Reestruturação do conteúdo de estudo

--- 00_0.sql
+++
@@ -1,7 +0,0 @@
-/*
-Tipos de comandos SQL:
-- DML (Data manipulation language): SELECT, INSERT, UPDATE, DELETE, MERGE;
-- DDL (Data definition language): CREATE, ALTER, DROP, RENAME, TRUNCATE, COMMENT;
-- DCL (Data control language): GRANT, REVOKE;
-- Transaction Control: COMMIT, ROLLBACK, SAVEPOINT;
-*/
